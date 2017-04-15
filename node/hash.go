8dab47e836591245163eb58972c1cd6b76d8645d
Rahul Sridhar <rsridhar@mit.edu>
2017-04-14 23:52:54 -0400

    Added some more files

---
+++ node/hash.go
@@ -0,0 +1,14 @@
+package node
+
+import (
+  "golang.org/x/crypto/scrypt"
+)
+
+//
+// Uses scrypt to hash data
+func hash(data []byte) []byte {
+  // TODO check that data is 80 bytes
+  // and do something if it isn't
+  dk, err = scrypt.Key(data, data, 1024, 1, 1, 32)
+  return dk
+}
