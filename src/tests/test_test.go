ae2e363f4e69706cab93b3cae45b8caba9591e92
Rahul Sridhar <rsridhar@mit.edu>
2017-05-10 22:56:37 -0400

    Added test w/ multiple clients

--- src/tests/test_test.go
+++ src/tests/test_test.go
@@ -3,14 +3,20 @@ package tests
 import (
   "testing"
   "fmt"
+  "node"
+  "clerk"
 )
 
-func TestOnePost(t *testing.T) {
+func TestOnePostSingleClient(t *testing.T) {
   fmt.Printf("Test: Basic setup with one miner, one user, one post ...\n")
+
+  node.DIFFICULTY = 1
+  node.BLOCK_SIZE_THRESHOLD = 1
+
   const nservers = 1
   const nclients = 1
   const unreliable = false
-  const tag = "basic_one_post"
+  const tag = "BasicOnePostSingleClient"
   cfg := make_config(t, tag, nservers, unreliable)
   defer cfg.cleanup()
 
@@ -24,21 +30,68 @@ func TestOnePost(t *testing.T) {
   fmt.Printf("  ... Passed\n")
 }
 
-func TestMultiplePosts(t *testing.T) {
+func TestMultiplePostsSingleClient(t *testing.T) {
   fmt.Printf("Test: Basic setup with one miner, one user, ten posts ...\n")
+
+  node.DIFFICULTY = 1
+  node.BLOCK_SIZE_THRESHOLD = 1
+
   const nservers = 1
   const nclients = 1
+  const nposts   = 10
   const unreliable = false
-  const tag = "basic_multiple_posts"
+  const tag = "BasicMultiplePostsSingleClient"
   cfg := make_config(t, tag, nservers, unreliable)
   defer cfg.cleanup()
 
+
   ck := cfg.makeClient(cfg.All())
 
-  for i := 1; i <= 10; i++ {
+  for i := 1; i <= nposts; i++ {
     ok := ck.Post(fmt.Sprintf("Post #%d", i))
     if !ok {
       t.Fatal("Attempted Post failed")
+    }
+  }
+
+  fmt.Printf("  ... Passed\n")
+}
+
+func TestMultiplePostsMultipleClients(t *testing.T) {
+  fmt.Printf("Test: Basic setup with one miner, ten users, ten posts each ...\n")
+
+  node.DIFFICULTY = 1
+  node.BLOCK_SIZE_THRESHOLD = 1
+
+  const nservers = 1
+  const nclients = 10
+  const nposts   = 10
+  const unreliable = false
+  const tag = "BasicMultiplePostsMultipleClients"
+  cfg := make_config(t, tag, nservers, unreliable)
+  defer cfg.cleanup()
+
+  ch := make(chan int)
+
+  for j := 1; j <= nclients; j++ {
+    ck := cfg.makeClient(cfg.All())
+    go func(ck *clerk.Clerk, j int) {
+      for i := 1; i <= nposts; i++ {
+        ok := ck.Post(fmt.Sprintf("Client #%d; Post #%d", j, i))
+        if !ok {
+          t.Fatal("Attempted Post failed")
+        }
+      }
+      ch <- j
+    }(ck, j)
+  }
+
+  count := 0
+  for {
+    <-ch
+    count++
+    if count == nclients {
+      break
     }
   }
 
