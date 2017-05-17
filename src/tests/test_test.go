9eb0f67de3160d83b572e570463f2bd06eb58d67
Rahul Sridhar <rsridhar@mit.edu>
2017-05-17 14:54:43 -0400

    whitespace!!

--- src/tests/test_test.go
+++ src/tests/test_test.go
@@ -1,46 +1,46 @@
 package tests
 
 import (
-  "testing"
-  "fmt"
+	"testing"
+	"fmt"
 )
 
 func TestOnePost(t *testing.T) {
-  fmt.Printf("Test: Basic setup with one miner, one user, one post ...\n")
-  const nservers = 1
-  const nclients = 1
-  const unreliable = false
-  const tag = "basic_one_post"
-  cfg := make_config(t, tag, nservers, unreliable)
-  defer cfg.cleanup()
+	fmt.Printf("Test: Basic setup with one miner, one user, one post ...\n")
+	const nservers = 1
+	const nclients = 1
+	const unreliable = false
+	const tag = "basic_one_post"
+	cfg := make_config(t, tag, nservers, unreliable)
+	defer cfg.cleanup()
 
-  ck := cfg.makeClient(cfg.All())
-  ok := ck.Post("First Post!!!1")
+	ck := cfg.makeClient(cfg.All())
+	ok := ck.Post("First Post!!!1")
 
-  if !ok {
-    t.Fatal("Attempted Post failed")
-  }
+	if !ok {
+		t.Fatal("Attempted Post failed")
+	}
 
-  fmt.Printf("  ... Passed\n")
+	fmt.Printf("  ... Passed\n")
 }
 
 func TestMultiplePosts(t *testing.T) {
-  fmt.Printf("Test: Basic setup with one miner, one user, ten posts ...\n")
-  const nservers = 1
-  const nclients = 1
-  const unreliable = false
-  const tag = "basic_multiple_posts"
-  cfg := make_config(t, tag, nservers, unreliable)
-  defer cfg.cleanup()
+	fmt.Printf("Test: Basic setup with one miner, one user, ten posts ...\n")
+	const nservers = 1
+	const nclients = 1
+	const unreliable = false
+	const tag = "basic_multiple_posts"
+	cfg := make_config(t, tag, nservers, unreliable)
+	defer cfg.cleanup()
 
-  ck := cfg.makeClient(cfg.All())
+	ck := cfg.makeClient(cfg.All())
 
-  for i := 1; i <= 10; i++ {
-    ok := ck.Post(fmt.Sprintf("Post #%d", i))
-    if !ok {
-      t.Fatal("Attempted Post failed")
-    }
-  }
+	for i := 1; i <= 10; i++ {
+		ok := ck.Post(fmt.Sprintf("Post #%d", i))
+		if !ok {
+			t.Fatal("Attempted Post failed")
+		}
+	}
 
-  fmt.Printf("  ... Passed\n")
+	fmt.Printf("  ... Passed\n")
 }
